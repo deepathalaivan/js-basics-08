29d5cd86ecfcba3ff3631a871f92afb77e20c08d
DivyaKannan6 <aaradiyaramanathan@gmail.com>
2022-12-16 17:42:07 -0500

    js basics

---
+++ js-basics-02-.js
@@ -0,0 +1,51 @@
+//function is a block of code, that does someting and return output
+//function functionName(){
+    //some logic
+    //}
+    //console.log(functionName())
+
+let x=10+3
+
+function addNumbers(a,b){
+    return a+b
+}
+console.log(addNumbers(10,20))
+
+function multipleNumbers(x,y){
+    return x*y
+}
+console.log(multipleNumbers(20,30))
+
+function findBiggest(a,b){
+    if(a>b) return a
+    else return b
+}
+console.log(findBiggest(100,200))
+
+function areaOfTriangle(b,h){
+    return 1/2*b*h
+}
+console.log(areaOfTriangle(10,20))
+
+function isOdd(a){
+    console.log('Is Odd' +a%2==1)
+
+}
+console.log
+
+//Pushing in array
+const countries =['Inida','uk','usa']
+console.log(countries)
+countries.push('aaa')
+console.log(countries)
+countries.unshift('canada')
+console.log(countries)
+
+//length of array
+
+function findLenthOfArray(aaa){
+    return aaa.length
+
+}
+const aaa=['fsgfsf','dffdf']
+console.log(findLengthOfArray(aaa))
---
+++ js-basics01-.js
@@ -0,0 +1,111 @@
+var name ='Divya'
+name='ola'
+
+var age =10
+var email='aaa@test.com'
+var address='123 m st'
+
+var sum = 10+20
+var x ='a'+10
+
+var foo ='hello'+'world'
+
+var isHuman = true
+isHuman = false
+
+var isVaccinated = false
+
+var ageOfPerson =40
+
+var x=1
+var y=2
+var z= x + y
+console.log(z)
+console.log('hello world')
+console.log(10+40)
+console.log('a'+40)
+
+var x=100
+var y =200
+x=x/2
+console.log (y*x)
+
+console.log(x==10)
+
+
+const s =100
+console.log(s)
+
+
+//if else condition
+var ss=10
+if (ss==10)
+{
+console.log('equals')
+}
+else
+{
+console.log('not equals')
+}
+
+//for loop
+var i=3
+for(var i=0;i<3;i++)
+{
+    console.log('equals')
+}
+
+//example ++
+var t=10
+t++
+console.log(t)
+
+//displaying next numbers
+for(var i=10;i<15;i++)
+{
+    console.log('equals: '+i)
+}
+
+//print hello world
+
+for(var i=0;i<=10;i++)
+{
+    console.log('hello world')
+}
+
+//array
+let names = []
+console.log(names)
+names=['aaa','adsfd','dfgdg']
+let ages=[1,2,3]
+console.log(names[0])
+console.log(names.indexOf('aaa'))
+
+
+//object will always be in {}
+//properites will be defined as key/value pair 
+//e.g: key:value
+//use . operator to access one property of a object
+// key should be camelcase and it should be in one word
+let car ={
+    make:'bmw',
+    model:'x5',
+    type:'suv'
+}
+
+let country ={
+    name:'india',
+    captial:'newdelhi',
+    languages:10,
+    state:28,
+    region : 4
+}
+console.log(country.languages)
+
+let stores = {
+    name : 'target',
+    numbers:100,
+    section:'garnaments'
+
+}
+console.log(stores.section)
