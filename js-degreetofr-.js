a4144537b24829722b355d81699c760668cc5926
deepathalaivan <deepathalaivan@gmail.com>
2023-01-22 10:39:26 -0600

    test

--- js-degreetofr-.js
+++ js-degreetofr-.js
@@ -1,8 +1,8 @@
 function cToF(celsius) 
 {
-var cTemp = celsius;
-var cToFahr = cTemp * 9 / 5 + 32;
-var message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
+var cTemp1 = celsius;
+var cToFahr1 = cTemp * 9 / 5 + 32;
+var message1 = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
 console.log(message);
 }
 cToF(60);
